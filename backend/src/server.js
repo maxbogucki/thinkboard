fff2de5079477349deb6672619eeb4ebb177e4a3
maxbogucki <bogucki.max@gmail.com>
2025-08-22 18:34:11 -0500

    Implement CRUD operations for notes API

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 5001;
 
 connectDB();
 
+// middleware to parse JSON bodies
+app.use(express.json());
+
 app.use("/api/notes", notesRoutes);
 
 app.listen(PORT, () => {
