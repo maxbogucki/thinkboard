17a4468b7d9c30ea31aa09e095f1605b1ac3685f
maxbogucki <bogucki.max@gmail.com>
2025-08-23 19:28:29 -0500

    feat: create HomePage component for displaying user notes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
-import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// middleware to parse JSON bodies
+app.use(cors({
+  origin: "http://localhost:5173",
+}));
 app.use(express.json());
-
 app.use(rateLimiter);
 
 app.use((req, res, next) => {
